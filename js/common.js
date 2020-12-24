6b5d16ddf75a8d2e0aaf2fbc018cae2bd4982af0
wnsdnn <a24369029@gmail.com>
2020-12-24 18:42:34 +0900

    wnsdnn

--- js/common.js
+++ js/common.js
@@ -1,1 +1,77 @@
 //write...
+window.onload = function(){
+    ballmove();
+}
+
+//공 움직이기
+
+let ball = document.querySelector('.ball');
+let x = 0;
+let y = 0;
+function playR(){
+    x += 1; 
+    ball.style.marginLeft = x+'px'; 
+}
+function playL(){
+    x -= 1;
+    ball.style.marginLeft = x+'px';
+}
+function playB(){
+    y -= 1;
+    ball.style.marginBottom = y+'px'
+}
+function playT(){
+    y += 1;
+    ball.style.marginBottom = y+'px';
+}
+let set = setInterval(playR,10)
+
+function ballmove(){
+    // 스페이스바 누를시 공 멈추기/ 움직이기
+    window.addEventListener('keydown', function(e){
+        let key = e.key;
+        let bl = true;
+        while(bl){
+            switch(key){
+                case 32:
+                    clearInterval(set)
+                    bl = false;
+                    break;
+            // 방향키로 공 움직이기
+                case 37:
+                    clearInterval(set)
+                    set = setInterval(playL,10)
+                    break;
+                case 38:
+                    clearInterval(set)
+                    set = setInterval(playT,10)
+                    break;
+                case 39:
+                    clearInterval(set)
+                    set = setInterval(playR,10)
+                    break;
+                case 40:
+                    clearInterval(set)
+                    set = setInterval(playB,10)
+                    break;
+            }
+        }
+        if(bl == false){
+            window.addEventListener('keydown', function(e){
+                let key = e.key;
+                if(key == 32){
+                    bl =true
+                }
+            })
+        }
+    })
+}
+//벽에 닿으면 반대 방향으로 튕기기
+
+// getBoundingClientRect
+
+
+
+//공이 멈춰 있을때 어떠한 조작이 안되게 하기
+
+
